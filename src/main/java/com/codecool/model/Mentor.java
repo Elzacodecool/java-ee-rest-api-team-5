904ae34ec73fdd8ca3d826ea6b23f4c11911ee2f
szymslow12 <szym.slow12@gmail.com>
2018-08-27 17:27:24 +0200

    add classRoomList -> getter/setter, constructors

--- src/main/java/com/codecool/model/Mentor.java
+++ src/main/java/com/codecool/model/Mentor.java
@@ -1,4 +1,25 @@
 package com.codecool.model;
 
-public class Mentor {
+import java.util.List;
+
+public class Mentor extends Person {
+
+    private List<ClassRoom> classRoomList;
+
+    public Mentor() {
+        super();
+    }
+
+    public Mentor(String name, String email, String phoneNumber, List<ClassRoom> classRoomList) {
+        super(name, email, phoneNumber);
+        this.classRoomList = classRoomList;
+    }
+
+    public List<ClassRoom> getClassRoomList() {
+        return classRoomList;
+    }
+
+    public void setClassRoomList(List<ClassRoom> classRoomList) {
+        this.classRoomList = classRoomList;
+    }
 }
