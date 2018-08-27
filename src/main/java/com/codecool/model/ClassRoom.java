ab5f0f9e75771a510a367c3007826ea346bb943f
szymslow12 <szym.slow12@gmail.com>
2018-08-27 17:26:31 +0200

    add ClassRoom ->className, studentsList, mentorsList and getters/setters

---
+++ src/main/java/com/codecool/model/ClassRoom.java
@@ -0,0 +1,44 @@
+package com.codecool.model;
+
+import java.util.List;
+
+public class ClassRoom {
+
+    private String className;
+
+    private List<Student> studentsList;
+
+    private List<Mentor> mentorsList;
+
+    public ClassRoom() {}
+
+    public ClassRoom(String className, List<Student> studentsList, List<Mentor> mentorsList) {
+        this.className = className;
+        this.studentsList = studentsList;
+        this.mentorsList = mentorsList;
+    }
+
+    public String getClassName() {
+        return className;
+    }
+
+    public void setClassName(String className) {
+        this.className = className;
+    }
+
+    public List<Student> getStudentsList() {
+        return studentsList;
+    }
+
+    public void setStudentsList(List<Student> studentsList) {
+        this.studentsList = studentsList;
+    }
+
+    public List<Mentor> getMentorsList() {
+        return mentorsList;
+    }
+
+    public void setMentorsList(List<Mentor> mentorsList) {
+        this.mentorsList = mentorsList;
+    }
+}
