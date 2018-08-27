680634fad83782c1f27a847a0edc1a92f50aa872
szymslow12 <szym.slow12@gmail.com>
2018-08-27 17:27:57 +0200

    add classRoom, personalMentor, constructors, getters/setters

--- src/main/java/com/codecool/model/Student.java
+++ src/main/java/com/codecool/model/Student.java
@@ -1,4 +1,34 @@
 package com.codecool.model;
 
-public class Student {
+public class Student extends Person {
+
+    private ClassRoom classRoom;
+
+    private Mentor personalMentor;
+
+    public Student() {
+        super();
+    }
+
+    public Student(String name, String email, String phoneNumber, ClassRoom classRoom, Mentor mentor) {
+        super(name, email, phoneNumber);
+        this.classRoom = classRoom;
+        this.personalMentor = mentor;
+    }
+
+    public ClassRoom getClassRoom() {
+        return classRoom;
+    }
+
+    public void setClassRoom(ClassRoom classRoom) {
+        this.classRoom = classRoom;
+    }
+
+    public Mentor getPersonalMentor() {
+        return personalMentor;
+    }
+
+    public void setPersonalMentor(Mentor personalMentor) {
+        this.personalMentor = personalMentor;
+    }
 }
