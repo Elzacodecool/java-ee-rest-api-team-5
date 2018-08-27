34737d393c243c1785e80ee586e10d71586d2a59
Elzacodecool <elliegolec@gmail.com>
2018-08-27 16:01:05 +0200

    Add annotations WebServlet

--- src/main/java/com/codecool/servlets/AdminServlet.java
+++ src/main/java/com/codecool/servlets/AdminServlet.java
@@ -1,6 +1,18 @@
 package com.codecool.servlets;
 
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
 
+@WebServlet(name="admin",
+        urlPatterns = {"/admin"})
 public class AdminServlet extends HttpServlet {
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        response.getWriter().println("Hello World aaaaaaaaaaaaaaaaaaa!");
+    }
 }
