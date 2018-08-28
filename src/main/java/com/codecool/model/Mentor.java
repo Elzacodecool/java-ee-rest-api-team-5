75d2ef4fa7c24609ff1b7fa649b825a7b1ea50dc
Elzacodecool <elliegolec@gmail.com>
2018-08-28 11:36:31 +0200

    Add languages to mentor

--- src/main/java/com/codecool/model/Mentor.java
+++ src/main/java/com/codecool/model/Mentor.java
@@ -1,21 +1,30 @@
 package com.codecool.model;
 
 import javax.persistence.Entity;
+import javax.persistence.ManyToMany;
 import java.util.List;
 
 @Entity
 public class Mentor extends Person {
-    private List<String> languages;
+    @ManyToMany
+    private List<Language> languages;
 
     public Mentor() {
         super();
     }
 
-    public Mentor(String name, String email, String phoneNumber, List<String> languages) {
+    public Mentor(String name, String email, String phoneNumber, List<Language> languages) {
         super(name, email, phoneNumber);
         this.languages = languages;
     }
 
+    public List<Language> getLanguages() {
+        return languages;
+    }
+
+    public void setLanguages(List<Language> languages) {
+        this.languages = languages;
+    }
     public List<ClassRoom> getClassRoomList() {
         return null;
     }
