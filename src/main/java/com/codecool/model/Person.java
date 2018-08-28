e5f7e6375946b66dd6a2a90675f9b07ba5e72548
szymslow12 <szym.slow12@gmail.com>
2018-08-28 11:42:55 +0200

    add @Entity, @Table to Type, add id field with @Id, @GeneratedValue

--- src/main/java/com/codecool/model/Person.java
+++ src/main/java/com/codecool/model/Person.java
@@ -1,6 +1,14 @@
 package com.codecool.model;
 
+import javax.persistence.*;
+
+@Entity
+@Table(name = "persons")
 public abstract class Person {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
     private String name;
 
