450a9defe37f609601c6a2ff3908f88750f51d6a
szymslow12 <szym.slow12@gmail.com>
2018-08-28 12:08:42 +0200

    add @Table to Type,add @ElementCollection, @CollectionTable to languages

--- src/main/java/com/codecool/model/Mentor.java
+++ src/main/java/com/codecool/model/Mentor.java
@@ -1,10 +1,16 @@
 package com.codecool.model;
 
+import javax.persistence.CollectionTable;
+import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
+import javax.persistence.Table;
 import java.util.List;
 
 @Entity
+@Table(name = "mentors")
 public class Mentor extends Person {
+    @ElementCollection
+    @CollectionTable(name = "mentor_languages")
     private List<String> languages;
 
     public Mentor() {
