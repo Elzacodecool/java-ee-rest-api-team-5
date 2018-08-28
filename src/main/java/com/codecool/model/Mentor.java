872d8756d33b97aa9969c6ef57b4f8972a0dce7e
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-08-28 10:52:19 +0200

    Remove Classrom field from student and mentor

--- src/main/java/com/codecool/model/Mentor.java
+++ src/main/java/com/codecool/model/Mentor.java
@@ -4,22 +4,13 @@ import java.util.List;
 
 public class Mentor extends Person {
 
-    private List<ClassRoom> classRoomList;
 
     public Mentor() {
         super();
     }
 
-    public Mentor(String name, String email, String phoneNumber, List<ClassRoom> classRoomList) {
+    public Mentor(String name, String email, String phoneNumber) {
         super(name, email, phoneNumber);
-        this.classRoomList = classRoomList;
     }
 
-    public List<ClassRoom> getClassRoomList() {
-        return classRoomList;
-    }
-
-    public void setClassRoomList(List<ClassRoom> classRoomList) {
-        this.classRoomList = classRoomList;
-    }
 }
