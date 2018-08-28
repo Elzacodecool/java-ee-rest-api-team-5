2887a496cb48e6132ed458b577f50b18d4265990
Elzacodecool <elliegolec@gmail.com>
2018-08-28 11:37:08 +0200

    Map person

--- src/main/java/com/codecool/model/Person.java
+++ src/main/java/com/codecool/model/Person.java
@@ -1,6 +1,14 @@
 package com.codecool.model;
 
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.Id;
+
+@Entity
 public abstract class Person {
+    @Id
+    @GeneratedValue
+    private int id;
 
     private String name;
 
