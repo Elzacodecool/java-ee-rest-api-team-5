6e311e82e12c17c410f58ed2b0a0541397df2fc3
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-08-28 11:11:35 +0200

    Populate data base

--- src/main/java/com/codecool/JPATest.java
+++ src/main/java/com/codecool/JPATest.java
@@ -5,12 +5,41 @@ import com.codecool.model.Mentor;
 import com.codecool.model.Student;
 
 import javax.persistence.EntityManager;
+import javax.persistence.EntityManagerFactory;
 import javax.persistence.EntityTransaction;
+import javax.persistence.Persistence;
 import java.util.ArrayList;
 import java.util.List;
 
 public class JPATest {
     public static void populateDb(EntityManager em) {
+
+        Mentor mentor = new Mentor("Marcin Izworski", "m.izworski@gmail.com","123456789");
+        Student student = new Student("Elzbieta Krzych", "elzbieta.krzych@gmail.com","123456789", mentor);
+
+        List<Student> studentList = new ArrayList<>();
+        studentList.add(student);
+
+        List<Mentor> mentorList = new ArrayList<>();
+        mentorList.add(mentor);
+
+        ClassRoom classA = new ClassRoom("webRoom", studentList,mentorList);
+
+        EntityTransaction transaction = em.getTransaction();
+
+        transaction.begin();
+        em.persist(student);
+        em.persist(mentor);
+        em.persist(classA);
+        transaction.commit();
+
+    }
+
+    public static void main(String[] args) {
+        EntityManagerFactory emf = Persistence.createEntityManagerFactory("codecoolPU");
+        EntityManager em = emf.createEntityManager();
+
+        populateDb(em);
 
     }
 }
