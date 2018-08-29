bd347d9d4b1df5204ece8db0165cd6b5bcadb8f0
Elzacodecool <elliegolec@gmail.com>
2018-08-29 15:46:35 +0200

    Implement get method

--- src/main/java/com/codecool/servlets/MentorServlet.java
+++ src/main/java/com/codecool/servlets/MentorServlet.java
@@ -1,9 +1,42 @@
 package com.codecool.servlets;
 
+import com.codecool.DAOFactory.MentorDAO;
+import com.codecool.model.Mentor;
+import com.google.gson.Gson;
+
+import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+import java.io.PrintWriter;
 
-@WebServlet(name="mentor",
-        urlPatterns = {"/mentor"})
+@WebServlet(name="mentors",
+        urlPatterns = {"/mentors/*"})
 public class MentorServlet extends HttpServlet {
+    private MentorDAO mentorDAO = new MentorDAO();
+
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+        String pathInfo = request.getPathInfo();
+        PrintWriter out = response.getWriter();
+        String employeeJsonString = getJsonStringMentors(pathInfo);
+
+        response.setContentType("application/json");
+        response.setCharacterEncoding("UTF-8");
+        out.print(employeeJsonString);
+        out.flush();
+
+    }
+
+    private String getJsonStringMentors(String pathInfo) {
+        if (pathInfo == null) {
+            return new Gson().toJson(mentorDAO.getAllMentors());
+        } else {
+            int id = Integer.valueOf(pathInfo.split("/")[1]);
+            Mentor mentor = mentorDAO.getMentor(id);
+            return new Gson().toJson(mentor);
+        }
+    }
 }
