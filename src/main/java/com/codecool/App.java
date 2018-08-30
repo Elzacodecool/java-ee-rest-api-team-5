5ede5d6f8e2094bd59bc93dab454c86c954bbb3b
szymslow12 <szym.slow12@gmail.com>
2018-08-30 11:22:10 +0200

    change in name in addServlet -> student to students

--- src/main/java/com/codecool/App.java
+++ src/main/java/com/codecool/App.java
@@ -17,7 +17,7 @@ public class App implements ServletContextListener {
     public void contextInitialized(ServletContextEvent servletContextEvent) {
         EntityManagerFactory factory = Persistence.createEntityManagerFactory("codecoolPU");
         ServletContext servletContext = servletContextEvent.getServletContext();
-        servletContext.addServlet("student", new StudentServlet(new StudentDAO(factory)));
+        servletContext.addServlet("students", new StudentServlet(new StudentDAO(factory)));
     }
 
     @Override
