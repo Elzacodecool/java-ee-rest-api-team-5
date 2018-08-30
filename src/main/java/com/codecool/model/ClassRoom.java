780b834455eba7fc33d41a2280cc09b22cdb9f1d
Elzbieta Krzych <elzbieta.krzych@gmail.com>
2018-08-30 12:09:57 +0200

    Remove spare imports

--- src/main/java/com/codecool/model/ClassRoom.java
+++ src/main/java/com/codecool/model/ClassRoom.java
@@ -2,9 +2,9 @@ package com.codecool.model;
 
 import javax.persistence.*;
 import java.util.ArrayList;
-import java.util.HashSet;
+
 import java.util.List;
-import java.util.Set;
+
 
 @Entity
 public class ClassRoom {
